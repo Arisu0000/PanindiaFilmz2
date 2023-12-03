b19841fb76dbd10c2fc6ec3479534ed6d4462cd8
Arisu0000 <130484247+Arisu0000@users.noreply.github.com>
2023-12-03 21:28:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -246,15 +246,14 @@ async def start(client, message):
     if f_caption is None:
         f_caption = f"{files.file_name}"
    l = await client.send_cached_media(
-   await asyncio.sleep(10)
-   await l.delete()
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
         reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://t.me/PanindiaFilmz') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
-                    
+  await asyncio.sleep(10)
+  await l.delete()                
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
 async def channel_info(bot, message):
