8b15e8ccdd1dfa21cb484fd3c49e3e22006c91e0
Arisu0000 <130484247+Arisu0000@users.noreply.github.com>
2023-12-03 22:14:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -249,7 +249,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/@GreyMattersYT') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://t.me/PanindiaFilmZ') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
     await asyncio.sleep(10)
